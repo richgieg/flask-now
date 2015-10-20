af6fc31b2cd36e7bac99695b9eade1142da65580
Richard Gieg <richgieg@users.noreply.github.com>
2015-10-20 00:10:46 -0700

    Add moment.js flask module

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 from flask.ext.script import Manager
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':

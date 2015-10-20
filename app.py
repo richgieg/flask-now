1a7e0bff5cc48d306c0ab21d9c4de16ff49024d6
Richard Gieg <richgieg@users.noreply.github.com>
2015-10-20 10:35:49 -0700

    Add custom error handlers

--- app.py
+++ app.py
@@ -14,6 +14,16 @@ manager = Manager(app)
 moment = Moment(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
